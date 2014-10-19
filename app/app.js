dbbd29129d5fea00fea18c31b3d74c9d34618c05
James O'Neill <hemebond@gmail.com>
2014-10-20 11:16:31 +1300

    Updated with new community, org and entitlement filters.

--- app/app.js
+++ app/app.js
@@ -4,24 +4,27 @@
 	angular
 		.module('SPConfig', ['monospaced.elastic'])
 		.controller('ConfigController', function($scope, $window, $compile) {
+
 			var createSecurityRolesRule = function() {
 				var CDATA = '## Add entitlements\n#set($entitlements = "")\n#macro(addEntitlementIfDefined $entitlement $roleValue)\n\t#if(${currentLoginContext.hasEntitlement($entitlement)})\n\t\t#if($entitlements != "")\n\t\t\t#set($entitlements = $entitlements + ",")\n\t\t#end\n\t\t#set($entitlements = $entitlements + $roleValue)\n\t#end\n#end\n';
 
-				for (var i = 0; i < $scope.securityRoles.length; i++) {
-					var role = $scope.securityRoles[i].name;
-					var entitlement = $scope.securityRoles[i].entitlement;
+				for (var i = 0; i < $scope.entitlements.length; i++) {
+					var e = $scope.entitlements[i];
 
-					if (role !== "" && entitlement !== "") {
-						CDATA += '#addEntitlementIfDefined("' + entitlement + '", "' + role + '")\n';
+					if (e !== '') {
+						CDATA += '#addEntitlementIfDefined("' + e + '", "' + e + '")\n';
 					}
 					else {
 						continue;
 					}
 				}
 
-				CDATA += '$entitlements';
-
-				return CDATA;
+				return CDATA + '$entitlements';
+			};
+			$scope.securityRolesAttributeMap = {
+				tname: 'ESAA2_SECURITYROLES',
+				cdata: createSecurityRolesRule,
+				help: 'User’s entitlements as a comma separated String.'
 			};
 
 			// Which application is this SP Config for?
@@ -30,109 +33,126 @@
 			// Attribute maps
 			$scope.attributeMaps = [
 				{
-					name: 'ESAA2_UUID',
+					tname: 'ESAA2_UUID',
 					rule: '$!{currentUser.uniqueId}',
 					help: 'Universally unique identifier (CDI or PDI?).'
 				},
 				{
-					name: 'ESAA2_CONTACTEMAIL',
-					cdata: true,
-					rule: '## Use CDI then PDI Email\n#set ($email = $!{currentLoginContext.getAttributeValue("email")})\n#if ("$email" == "" || ! $email)\n\t#set ($email = $!{currentUser.getAttributeValue("email")})\n#end\n$!{email}',
+					tname: 'ESAA2_CONTACTEMAIL',
+					cdata: '## Use CDI then PDI Email\n#set ($email = $!{currentLoginContext.getAttributeValue("email")})\n#if ("$email" == "" || ! $email)\n\t#set ($email = $!{currentUser.getAttributeValue("email")})\n#end\n$!{email}',
 					help: 'Release user’s Contextual email address (CDI) if present otherwise his personal email address (PDI).'
 				},
 				{
-					name: 'ESAA2_PREFERREDNAME',
-					cdata: true,
-					rule: '## Use CDI then PDI PreferredName\n#set ($preferredName = $!{currentLoginContext.getAttributeValue("preferredName")})\n#if ("$preferredName" == "" || ! $preferredName)\n\t#set ($preferredName = $!{currentUser.getAttributeValue("preferredName")})\n#end\n$!{preferredName}',
+					tname: 'ESAA2_PREFERREDNAME',
+					cdata: '## Use CDI then PDI PreferredName\n#set ($preferredName = $!{currentLoginContext.getAttributeValue("preferredName")})\n#if ("$preferredName" == "" || ! $preferredName)\n\t#set ($preferredName = $!{currentUser.getAttributeValue("preferredName")})\n#end\n$!{preferredName}',
 					help: 'Release user’s Contextual preferred name (CDI) if present otherwise his personal (PDI) one.'
 				},
 				{
-					name: 'ESAA2_LOGGEDINCONTEXTNAME',
+					tname: 'ESAA2_LOGGEDINCONTEXTNAME',
 					rule: '$!{currentUser.organizationName}',
 					help: 'The name of the context, e.g., My School.'
 				},
 				{
-					name: 'ESAA2_LOGGEDINCONTEXTID',
+					tname: 'ESAA2_LOGGEDINCONTEXTID',
 					rule: '$!{currentOrganization.getAttributeValue("partyID")}',
 					help: 'The ID of the context, e.g., 12345.'
 				},
 				{
-					name: 'ESAA2_TITLE',
-					rule: '$!{currentUser.getAttributeValue("title")}',
+					tname: 'ESAA2_TITLE',
+					cdata: '## Use CDI then PDI Title\n#set ($title = $!{currentLoginContext.getAttributeValue("title")})\n#if ("$title" == "" || ! $title)\n#set ($title = $!{currentUser.getAttributeValue("title")})\n#end\n$!{title}',
 					help: 'User’s PDI attribute value.'
-				},
-				{
-					name: 'ESAA2_SECURITYROLES',
-					cdata: true,
-					rule: createSecurityRolesRule,
-					help: 'User’s entitlements as a comma separated String.'
 				}
 			];
 
 			// The entitlements and roles for the SECURITY_ROLES attribute map
-			$scope.securityRoles = [
-				{
-					name: 'APP_ROLE',
-					entitlement: 'APP_ENTITLEMENT'
-				},
-				{
-					name: '',
-					entitlement: ''
-				}
-			];
+			$scope.entitlements = ['APP_ENTITLEMENT', ''];
+
+			$scope.addEntitlementFormFields = function() {
+				$scope.entitlements.push('');
+			};
 
 			// Beautify the XML when displaying it?
 			$scope.beautify = true;
-
-			$scope.addEntitlementFormFields = function() {
-				$scope.securityRoles.push({name: '', entitlement: ''});
-			};
 
 			var addAttributeMap = function(xml, o) {
 				var attributeMapClass = xml.getElementsByTagName('idaptiveAttributeMapClass')[0];
 				var attributeTargets = xml.getElementsByTagName('idaptiveAttributeMapNVTargetName');
 				var referenceNumber = attributeTargets.length;
 
-				var eName = xml.createElement('idaptiveAttributeMapNVTargetName');
-				var eNameText = xml.createTextNode("{" + referenceNumber + "}" + o['name']);
-				eName.appendChild(eNameText);
+				if (o.name) {
+					var eName = xml.createElement('idaptiveAttributeMapNVName');
+					var eNameText = xml.createTextNode("{" + referenceNumber + "}" + o['name']);
+					eName.appendChild(eNameText);
+				}
+
+				var eTargetName = xml.createElement('idaptiveAttributeMapNVTargetName');
+				var eTargetNameText = xml.createTextNode("{" + referenceNumber + "}" + o['tname']);
+				eTargetName.appendChild(eTargetNameText);
 
 				var eRule = xml.createElement('idaptiveAttributeMapNVRule');
+				var eRuleText = xml.createTextNode("{" + referenceNumber + "}");
 
-				if (o['cdata'] !== undefined && o['cdata'] === true) {
-					var eRuleText = xml.createTextNode("{" + referenceNumber + "}");
+				if (o['cdata'] !== undefined) {
+					var eRuleCDATA = null;
 
-					if (typeof o['rule'] === 'function') {
-						var fn = o['rule'];
-						var eRuleCDATA = xml.createCDATASection(fn());
+					// We can use a function to create the
+					// rule data/string
+					if (typeof o['cdata'] === 'function') {
+						eRuleCDATA = xml.createCDATASection(o['cdata']());
 					}
 					else {
-						var eRuleCDATA = xml.createCDATASection(o['rule']);
+						eRuleCDATA = xml.createCDATASection(o['cdata']);
 					}
 
 					eRule.appendChild(eRuleText);
 					eRule.appendChild(eRuleCDATA);
 				}
 				else {
-					var eRuleText = xml.createTextNode("{" + referenceNumber + "}" + o['rule']);
+					eRuleText.data += o['rule'];
 					eRule.appendChild(eRuleText);
 				}
 
+				if (o.name) {
+					attributeMapClass.appendChild(eName);
+				}
+
 				attributeMapClass.appendChild(eRule);
-				attributeMapClass.appendChild(eName);
+				attributeMapClass.appendChild(eTargetName);
 			};
 
 			var createConfig = function(conf) {
 				var parser = new DOMParser();
-				var template = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><idaptiveServiceProviderClass><idaptiveServiceProviderName></idaptiveServiceProviderName><attributemaps><idaptiveAttributeMapClass><idaptiveAttributeMapName>all_orgs_except_public</idaptiveAttributeMapName><idaptiveOrganizationDN>cn=*,objectClass=idaptiveOrganizationClass$,cn=communities,ou=idaptive-config</idaptiveOrganizationDN><idaptiveCommunityDN>idaptiveCommunityDescription=~Public$,objectClass=idaptiveCommunityClass$,cn=communities,ou=idaptive-config</idaptiveCommunityDN></idaptiveAttributeMapClass></attributemaps></idaptiveServiceProviderClass>";
+				var template = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><idaptiveServiceProviderClass><idaptiveServiceProviderName></idaptiveServiceProviderName><attributemaps><idaptiveAttributeMapClass><idaptiveAttributeMapName>all_orgs_except_public_and_central_admin</idaptiveAttributeMapName><idaptiveOrganizationDN>idaptiveOrganizationDescription=~Central Admin*,objectClass=idaptiveOrganizationClass$,cn=communities,ou=idaptive-config</idaptiveOrganizationDN><idaptiveCommunityDN>idaptiveCommunityDescription=~Public$,objectClass=idaptiveCommunityClass$,cn=communities,ou=idaptive-config</idaptiveCommunityDN></idaptiveAttributeMapClass></attributemaps></idaptiveServiceProviderClass>";
 				var xml = parser.parseFromString(template, "text/xml");
 
 				// Update the idaptiveServiceProviderName element
+				// with the application name
 				var serviceProviderNameText = xml.createTextNode(conf['applicationName']);
 				xml.getElementsByTagName('idaptiveServiceProviderName')[0].appendChild(serviceProviderNameText);
 
-				for (var i = 0; i < conf.attributeMaps.length; i++) {
+				var i = 0;
+				// Add the attribute maps
+				for (i = 0; i < conf.attributeMaps.length; i++) {
 					addAttributeMap(xml, conf.attributeMaps[i]);
+				}
+
+				// Create an attribute map for the security roles, aka, entitlements
+				if ($scope.securityRolesAttributeMap.enabled === true) {
+					addAttributeMap(xml, $scope.securityRolesAttributeMap);
+
+					// Add attribute maps for filtering CDIs on entitlements
+					// i.e., only show CDIs that has one of the entitlements
+					for (i = 0; i < $scope.entitlements.length; i++) {
+						var e = $scope.entitlements[i];
+
+						if (e !== '') {
+							addAttributeMap(xml, {
+								name: "__INTERNAL_USE_ONLY__",
+								tname: "__cdi_enabling_entitlement_id__",
+								rule: e
+							});
+						}
+					}
 				}
 
 				return xml;
